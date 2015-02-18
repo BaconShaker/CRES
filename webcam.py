a7b5e0f843bcb83b02f3fac2fbc2022bb6112b62
rshintani <rshintani@gmail.com>
2015-02-18 11:22:17 -0600

    Working on getting individual faceboxes to show up

--- webcam.py
+++ webcam.py
@@ -6,6 +6,7 @@ cascPath = '/home/robby/Downloads/opencv-3.0.0-beta/data/haarcascades/haarcascad
 faceCascade = cv2.CascadeClassifier(cascPath)
 eye_cascade = cv2.CascadeClassifier('/home/robby/Downloads/opencv-3.0.0-beta/data/haarcascades/haarcascade_eye.xml')
 
+# Try to make the while portion a method so it returns individual boxes! 
 
 video_capture = cv2.VideoCapture(0)
 
@@ -13,7 +14,11 @@ while True:
     # Capture frame-by-frame
     ret, frame = video_capture.read()
 
+    print ret
+    
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    print gray
 
     faces = faceCascade.detectMultiScale(
         gray,
@@ -23,13 +28,20 @@ while True:
         # flags=cv2.HAAR_SCALE_IMAGE
     )
 
+    # img = cv2.CloneImage(frame)
+    # print img
+
     print "There are ", len(faces) , 'faces found!'   
     # Draw a rectangle around the faces
 
     people = []
 
     for (x, y, w, h) in faces:
-        face = cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+        
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+        # person = ()
+
+        # roi_gray = cv2.cvtColor( person , cv2.COLOR_BGR2GRAY)
 
         roi_gray = gray[y:y+h, x:x+w]
         roi_color = frame[y:y+h, x:x+w]
@@ -38,9 +50,25 @@ while True:
             cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
 
         # Only update the display if there's two eyes in the 'face' rectangle
-        if len(eyes) == 2:
-            cv2.imshow('face', roi_color )
-            print "There are ", len(roi_gray) , 'eye pairs found!' , face
+        if len(eyes) != 0:
+            # print cv2.imdecode(roi_color ,  3)
+            people.append( roi_color )
+            # cv2.imshow( 'YES' , frame[y: y+h, x: x+w])
+
+
+
+    to_show = [(x,y,w,h) for person in people]
+    count = 0 
+    for thing in to_show:
+        print x
+        print 'thing' ,  thing
+        count += 1
+
+        cv2.imshow("I've just seen a face"+ str(count), frame[y: y+h, x: x+w])
+        cv2.waitKey(10)
+
+
+        # print "There are ", len(roi_gray) , 'eye pairs found!' 
 
     # Display the resulting frame
     
