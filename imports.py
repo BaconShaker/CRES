833b0fcae2806ad00c37c612b26b037c66a3f816
rshintani <rshintani@gmail.com>
2015-02-24 18:22:50 -0600

    Initial Commit import.py

---
+++ imports.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+
+from tabulate import tabulate #makes the nice tables
+import os.path #pathfinder
+import time #this is for any delays I want
+import sys
+import os
+from os import listdir
+import csv
+import urllib
+import urllib2
+from bs4 import BeautifulSoup		# pip install BeautifulSoup4
+from Tkinter import *
+import ttk
+from datetime import date
