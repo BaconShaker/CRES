6545e1b4312909345a1c2ea1c439dbfcb4b39269
Spartikus <rshintani@gmail.com>
2015-04-13 00:46:30 -0500

    Initial Commit

---
+++ threads/thread_try.py
@@ -0,0 +1,4 @@
+#!/bin/usr/python
+# This is my attempt at setting up a class\
+# then use the module to call if  __name__ == "main" do something\
+# and see if this fixes the problem in the root.mainloop() in the ttk GUI.
