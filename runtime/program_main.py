e4314549977d06bfa3049ff2539d2671080ba9b6
Spartikus <rshintani@gmail.com>
2015-05-05 14:14:10 -0500

    Initial

---
+++ runtime/program_main.py
@@ -0,0 +1,27 @@
+# --------------------  --------------------------
+# Height (DEPARTURE):   21
+# Pickup Score          34.38
+# Collectable Material  19.84
+# Height (ARRIVAL):     32
+# Duration:             0
+# Service Fee           0.15
+# Gallons on Departure  122.18
+# Notes:                0
+# Donation Rate         0.06
+# Miles in Route        7.5
+# Stops on Route        1
+# Total Distance        7.5
+# Date                  2015-04-17 16:41:52.958848
+# Fuel Surcharge        2.465625
+# Gallons on Arrival    186.18
+# Number of Stops       1
+# Contact Person        Robby Shintani
+# Expected Revenue      4.17
+# Diesel Price          2.63
+# Oil Price             0.21
+# Contact Email         rshintani@gmail.com
+# Gallons Collected     64.0
+# Expected Income       2.976
+# Quality (0 - 100):    4
+# Expected Donation     1.1904
+# --------------------  --------------------------
